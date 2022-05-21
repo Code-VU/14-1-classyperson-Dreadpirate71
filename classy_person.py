730ad47655f75a07b206fbcd5c1216c17bb090a3
James Remus <James.Remus@veteransunited.com>
2022-05-21 08:40:46 -0500

    create class for name and age

--- classy_person.py
+++ classy_person.py
@@ -9,6 +9,29 @@ don't forget the use of 'self' and to have the methods:
 
 class Person:
 
+    age = 0
+    name = ""
+
+    #initialize name and age
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    #method to increase age by 1
+    def increase_age(self):
+        self.age = self.age + 1
+        print(self.age)
+
+    #method to print greeting
+    def say_greeting(self):
+        print("Hello world! My name is", self.name)
+    
+    #method to count up to age by 1
+    def count_to_age(self):
+        count = 1
+        while count <= self.age:
+            print (count)
+            count = count + 1
 
 
 
